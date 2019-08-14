f00fe10a4180c543e9d7f7707b2311798b3c304a
Markus Maasikas <markus@hotmail.com>
2019-08-13 20:05:15 -0400

    updated projects

--- src/component/projects.jsx
+++ src/component/projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Vmarine from "../img/Vmarine.jpg";
-import aguaDeLuz from "../img/aguaDeLuz.png";
-import todo from "../img/todolist.png";
+import burger from "../img/burger.png";
+import crystal from "../img/crystal.png";
+import Habits from "../img/Habits66.png";
 import PropTypes from "prop-types";
 
 export default class Projects extends React.Component {
@@ -110,22 +110,22 @@ export default class Projects extends React.Component {
 
 				{/*hidden modals*/}
 				<div id="gallery-card">
-					<div id="vmarine" className="modal-card">
+					<div id="burger" className="modal-card">
 						<div className="visual">
-							<img src={Vmarine} alt=""/>
+							<img src={burger} alt=""/>
 						</div>
 						<div className="modal-info">
-							<h2>Vmarine</h2>
+							<h2>MVC Burger</h2>
 							<div className="modal-description">
 								<ul>
 									<li>
-										Prototype marine web-application to
-										provide information about progress of
-										services and prices.
+									A full stack, MVC (Model-View-Controller) application that allows you to create burgers (POST),
+									view a list of burgers available to eat (GET),
+									devour burgers (PUT), and throw them away after devoured (DELETE).
 									</li>
 									<li>
-										Designed with HTML5, CSS3, Bootstrap,
-										Webpack, Wordpress, and ReactJS.
+										Designed with HTML5, CSS, Bootstrap,
+										JS, MySQL, Express, and Handlebars.
 									</li>
 								</ul>
 							</div>
@@ -157,27 +157,28 @@ export default class Projects extends React.Component {
 
 					<div id="agualuz" className="modal-card">
 						<div className="visual">
-							<img src={aguaDeLuz} alt=""/>
+							<img src={crystal} alt=""/>
 						</div>
 						<div className="modal-info">
-							<h2>Agua De Luz</h2>
+							<h2>Crystal Collectors Game</h2>
 							<div className="modal-description">
 								<ul>
 									<li>
-										Prototype website to promote health
-										natural-made products to increase brand
-										and online presence.
+									At the start of the game, you are given a random number.
+									This number is the Target number that you need to reach to win.
+	                 To reach the Target number, click a crystal, which adds a specific number of points to Your number.
+                   To win the game, Your number must match the Target number. If you go over the Target number, you lose.
+                   The value of each crystal is hidden from you until you click it.
 									</li>
 									<li>
-										Developed with HTML5, CSS3, Bootstrap,
-										Webpack, Parallax Effect,
-										SmoothScrolling(UI), and VanillaJS.
+										Developed with HTML5, CSS, Bootstrap,
+										Javascript, and JQuery.
 									</li>
 								</ul>
 							</div>
 							<div className="modal-bottom">
 								<a
-									href="./AguaDeLuz/index.html"
+									href="./estomm01.github.io/unit-4-game/"
 									target="_blank">
 									<h3>View live demo</h3>
 								</a>
@@ -207,7 +208,7 @@ export default class Projects extends React.Component {
 
 					<div id="todolist" className="modal-card">
 						<div className="visual">
-							<img src={todo} alt=""/>
+							<img src={Habits} alt=""/>
 						</div>
 						<div className="modal-info">
 							<h2>To-Do List</h2>
